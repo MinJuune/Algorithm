22fad21e232af0b1f2d5a53d530732b59e8f65aa
MinJuune <rnrmf1121@gmail.com>
2024-03-28 10:54:30 +0900

    fibo_dp

---
+++ Algorithm/fibo_dp.cpp
@@ -0,0 +1,29 @@
+// 피보나치
+
+#include <iostream>
+using namespace std;
+
+int dp[26];
+
+int fibo(int n) {
+    for (int i = 0; i < n + 1; i++) {
+        if (i <= 1) {
+            dp[i] = i;
+        }
+        else {
+            dp[i] = (dp[i - 1] + dp[i - 2]);
+        }
+    }
+
+    return dp[n];
+}
+
+int main() {
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    cout << fibo(n);
+}
