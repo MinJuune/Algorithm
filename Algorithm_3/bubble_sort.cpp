5d9bf74e9207887fd6ea714c1ca51d0e88cb9f1f
MinJuune <rnrmf1121@gmail.com>
2024-06-25 00:49:51 +0900

    docs:bubble sort 백준아님

---
+++ Algorithm_3/bubble_sort.cpp
@@ -0,0 +1,38 @@
+// 백준 버블소트 풀이 아님
+// 백준은 이렇게 풀면 시간초과남
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[500001];
+
+void bubble_sort(int n){
+    int cnt=0;
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                cnt++;
+                
+            }
+        }
+    }
+
+    cout<<cnt;
+}
+
+int main(){
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int N;
+    cin>>N;
+
+    for(int i=0;i<N;i++){
+        cin>>arr[i];
+    }
+
+    bubble_sort(N);
+}
