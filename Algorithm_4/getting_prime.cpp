c97ec4b73f64ccec312fca6e33844b0df4f62dfb
MinJuune <rnrmf1121@gmail.com>
2024-07-03 14:35:23 +0900

    docs:getting_prime

---
+++ Algorithm_4/getting_prime.cpp
@@ -0,0 +1,48 @@
+// 백준 소수 구하기
+
+/*
+    그냥 소수를 구하면 시간초과 나므로
+    에라토스테네스의 체 구현가기 
+    범위 내에서 i=2부터 i*i<num까지
+    i를 자기의 2배수부터 모든 배수 0으로 바꿈
+    i를 i*i<num 될때까지 증가시키면서 0으로 바꿈
+*/
+
+#include <iostream>
+using namespace std;
+#include <algorithm>
+
+int arr[1000001];
+
+void solve(int m, int n){
+    for(int i=2;i<=n;i++){
+        arr[i]=i;
+    }
+
+    for(int i=2;i*i<=n;i++){
+        if(arr[i]==0){
+            continue;
+        }
+        else{   
+            for(int j=i*2;j<=n;j=j+i){
+                arr[j]=0;
+            }
+        }
+    }
+
+    for(int i=m;i<=n;i++){
+        if(arr[i]!=0){
+            cout<<i<<'\n';
+        }
+    }
+}
+
+int main(){
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+
+    int M,N;
+    cin>>M>>N;
+
+    solve(M,N);
+}
--- jusp.cpp
+++ jusp.cpp
@@ -2,40 +2,35 @@
 using namespace std;
 #include <algorithm>
 
+bool isPrime(int num){
+    for(int i=2;i<=num/2;i++){
+        if(num%i==0){
+            return false;
+        }
+        else{
+            continue;
+        }
+    }
+    return true;
+}
+
+void solve(int m, int n){
+    for(int i=m;i<=n;i++){
+        if(isPrime(i)){
+            cout<<i<<'\n';
+        }
+    }
+}
+
 int main(){
     cin.tie(NULL);
     cin.sync_with_stdio(false);
 
-    int arr[9];
-    int sum=0;
-    int target;
-    for(int i=0;i<9;i++){
-        cin>>arr[i];
-        sum+=arr[i];
-    }
+    int M,N;
+    cin>>M>>N;
 
-    target=sum-100;
+    solve(M,N);
 
-    int fake1, fake2;
-    for(int i=0;i<9;i++){
-        for(int j=0;j<9;j++){
-            if(i!=j){
-                if(arr[i]+arr[j]==target){
-                    fake1=i;
-                    fake2=j;
-                    break;
-                }
-            }
-        }
-    }
-
-    arr[fake1]=0;
-    arr[fake2]=0;
-    sort(arr,arr+9);
-    for(int i=0;i<9;i++){
-        if(arr[i]!=0){
-            cout<<arr[i]<<endl;
-        }
-    }
+    return 0;
 
 }
